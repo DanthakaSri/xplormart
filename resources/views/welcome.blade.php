8d6e775301166300b4ec9c2a09937334c1d7082d
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-08-31 02:32:26 +0930

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ExploreMart
                 </div>
 
                 <div class="links">
