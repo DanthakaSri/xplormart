77fc08d94282813ed77d3bc28e541277e0b5525b
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-08-31 02:36:04 +0930

    Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,12 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://exploremart.herokuapp.com/">Coming Soon</a>
+
                 </div>
             </div>
         </div>
