3f5bc2908202d435da454d62f614c299bf3c3e3f
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-09-13 00:33:19 +0930

    Home Page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Scripts -->
-{{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+<script src="{{ asset('js/app.js') }}" defer></script>
 
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}" defer></script>
 <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}" defer></script>
